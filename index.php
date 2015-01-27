44fd5d629deba23c88e68b9eb651a85729001b83
robe01 <topdoghalox@hotmail.com>
2015-01-27 18:08:41 +0000

    Version 2.1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     get_header(); 
 ?>
     <main>
-
+   
     </main>
 <?php
     get_footer();
---
+++ single.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ */
+
+get_header(); ?>
+
+    <main></main>
+
+<?php get_footer(); ?>
